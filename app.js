c32eb9fe2549c0a6f18786d0bde748bbbe7e64fb
faadhi <75142042+oxgr@users.noreply.github.com>
2022-06-14 13:50:44 -0400

    moved all globals to model

--- app.js
+++ app.js
@@ -7,144 +7,141 @@ const Stats = require( 'stats.js' );
 const { Camera } = require( '@mediapipe/camera_utils' );
 const drawUtils = require( '@mediapipe/drawing_utils/drawing_utils.js' )
 
+const model = {};
 
-// Settings
+main( model );
 
-let settings = JSON.parse( fs.readFileSync( __dirname + "/settings.json", "utf8" ) );
+function main( model ) {
 
-const params = {
-  global: {
-    showStats: true,
-  },
-  input: {
-    source: '',
-    mirror: true,
-    rotate: 0,
-    availableSources: {
-      video: {},
-      audio: {}
+  // Settings
+
+  model.settings = JSON.parse( fs.readFileSync( __dirname + "/settings.json", "utf8" ) );
+
+  model.params = {
+    global: {
+      showStats: true,
+    },
+    input: {
+      source: '',
+      mirror: true,
+      rotate: 0,
+      availableSources: {
+        video: {},
+        audio: {}
+      }
+    },
+    pose: {
+      options: {
+        modelComplexity: 1,
+        smoothLandmarks: true,
+        enableSegmentation: true,
+        smoothSegmentation: true,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5
+      }
+    },
+    draw: {
+      landmarks: true,
+      segmentationMask: false,
+      landmarkSize: 4,
+      connectorSize: 4,
+    },
+    osc: {
+      enable: true,
+      send_format: 'sendPosesJSON',
+      host: 'localhost',
+      port: '9527',
+      msgsPerSecond: 30,
     }
-  },
-  pose: {
-    options: {
-      modelComplexity: 1,
-      smoothLandmarks: true,
-      enableSegmentation: true,
-      smoothSegmentation: true,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+  }
+
+  // HTML
+
+  model.html = {};
+
+  model.html.videoElement = document.getElementById( 'input_video' );
+  model.html.videoCanvasElement = document.getElementById( 'input_video_canvas' );
+  model.html.videoCanvasCtx = model.html.videoCanvasElement.getContext( '2d' );
+  model.html.canvasElement = document.getElementById( 'output_canvas' );
+  model.html.canvasCtx = model.html.canvasElement.getContext( '2d' );
+
+  model.html.videoElement.onloadeddata = function () {
+
+    const w = model.html.videoElement.videoWidth
+    const h = model.html.videoElement.videoHeight;
+  
+    console.log( "camera dimensions", w, h );
+  
+    model.html.videoCanvasCtx.canvas.width = w;
+    model.html.videoCanvasCtx.canvas.height = h;
+    model.html.canvasCtx.canvas.width = w;
+    model.html.canvasCtx.canvas.height = h;
+  
+    ipcRenderer.send( 'resize', document.body.innerWidth, document.body.innerHeight ); //w, h);
+  
+  }
+
+  document.body.addEventListener( "keypress", onKeyPress );
+
+  // OSC
+
+  model.osc = openOSC( model.params.osc.host, model.params.osc.port );
+  model.oscTimer = Date.now();
+
+  model.keypointNames = [
+    'nose',
+    'left_eye_inner', 'left_eye', 'left_eye_outer',
+    'right_eye_inner', 'right_eye', 'right_eye_outer',
+    'left_ear', 'right_ear',
+    'mouth_left', 'mouth-right',
+    'left_shoulder', 'right_shoulder',
+    'left_elbow', 'right_elbow',
+    'left_wrist', 'right_wrist',
+    'left_pinky', 'right_pinky',
+    'left_index', 'right_index',
+    'left_thumb', 'right_thumb',
+    'left_hip', 'right_hip',
+    'left_knee', 'right_knee',
+    'left_ankle', 'right_ankle',
+    'left_heel', 'right_heel',
+    'left_foot_index', 'right_foot_index'
+  ]
+
+  // Audio playback to ensure camera keeps rendering even when window is not in focus
+  generateAudioElement( `${__dirname}/silent.mp3` );
+
+  // GUI
+
+  getStream( model.params.input.source, model.html.videoElement )
+  .then( getDevices( model.params.input.availableSources.video ) )
+  .then( generateGUI( model.params ) );
+
+  // Stats
+
+  model.stats = new Stats();
+  model.stats.showPanel( 0 );
+  document.body.appendChild( model.stats.dom );
+
+  // BlazePose
+
+  model.pose = new Pose.Pose( {
+    locateFile: ( file ) => {
+      return `${__dirname}/node_modules/@mediapipe/pose/${file}`;
     }
-  },
-  draw: {
-    landmarks: true,
-    segmentationMask: false,
-    landmarkSize: 4,
-    connectorSize: 4,
-  },
-  osc: {
-    enable: true,
-    send_format: 'sendPosesJSON',
-    host: 'localhost',
-    port: '9527',
-    msgsPerSecond: 30,
-  }
-}
+  } );
 
-const keypointNames = [
-  'nose',
-  'left_eye_inner', 'left_eye', 'left_eye_outer',
-  'right_eye_inner', 'right_eye', 'right_eye_outer',
-  'left_ear', 'right_ear',
-  'mouth_left', 'mouth-right',
-  'left_shoulder', 'right_shoulder',
-  'left_elbow', 'right_elbow',
-  'left_wrist', 'right_wrist',
-  'left_pinky', 'right_pinky',
-  'left_index', 'right_index',
-  'left_thumb', 'right_thumb',
-  'left_hip', 'right_hip',
-  'left_knee', 'right_knee',
-  'left_ankle', 'right_ankle',
-  'left_heel', 'right_heel',
-  'left_foot_index', 'right_foot_index'
-]
+  model.pose.setOptions( model.params.pose.options );
+  model.pose.onResults( onResults );
 
-let timer = Date.now();
+  // Camera  
 
-// OSC
+  model.camera = new Camera( model.html.videoElement, {
+    onFrame: onFrame,
+    width: model.html.videoElement.videoWidth,
+    height: model.html.videoElement.videoHeight
+  } );
 
-let osc = openOSC( params.osc.host, params.osc.port );
-
-// HTML
-
-document.body.addEventListener( "keypress", onKeyPress )
-
-const videoElement = document.getElementById( 'input_video' );
-const videoCanvasElement = document.getElementById( 'input_video_canvas' );
-const videoCanvasCtx = videoCanvasElement.getContext( '2d' );
-
-const canvasElement = document.getElementById( 'output_canvas' );
-const canvasCtx = canvasElement.getContext( '2d' );
-
-//// Audio playback to ensure camera keeps rendering even when window is not in focus
-generateAudioElement( `${__dirname}/silent.mp3` );
-
-// GUI
-
-getStream( params.input.source, videoElement )
-  .then( getDevices( params.input.availableSources.video ) )
-  .then( generateGUI( params ) );
-
-// Stats
-
-let stats = new Stats();
-stats.showPanel( 0 );
-document.body.appendChild( stats.dom );
-
-// Camera
-
-var [ w, h ] = [ 0, 0 ];
-
-videoElement.onloadeddata = function () {
-
-  [ w, h ] = [ videoElement.videoWidth, videoElement.videoHeight ];
-
-  console.log( "camera dimensions", w, h );
-
-  videoCanvasCtx.canvas.width = w;
-  videoCanvasCtx.canvas.height = h;
-  canvasCtx.canvas.width = w;
-  canvasCtx.canvas.height = h;
-
-  ipcRenderer.send( 'resize', document.body.innerWidth, document.body.innerHeight ); //w, h);
-
-}
-
-// BlazePose
-
-const pose = new Pose.Pose( {
-  locateFile: ( file ) => {
-    return `${__dirname}/node_modules/@mediapipe/pose/${file}`;
-  }
-} );
-
-pose.setOptions( params.pose.options );
-pose.onResults( onResults );
-
-// Camera  
-
-const camera = new Camera( videoElement, {
-  onFrame: onFrame,
-  width: w,
-  height: h
-} );
-
-camera.start();
-
-
-main( params );
-
-function main( params ) {
+  model.camera.start();
 
 }
 
@@ -153,26 +150,35 @@ function main( params ) {
 async function onFrame() {
 
   // Settings args because @mediapipe/camera_utils returns a specific to onFrame and can't take a function with custom ones.
-  // Even though this args takes globals, this is here so refactoring to a pure function is easier in the future if we don't use @mediapipe/camera_utils.
+  // Even though this args takes globals, it's here so refactoring to a pure function is easier in the future if we don't use @mediapipe/camera_utils.
   const args = {
-    video: videoElement,
-    canvas: videoCanvasElement,
-    context: videoCanvasCtx,
-    pose: pose
+    video: model.html.videoElement,
+    canvas: model.html.videoCanvasElement,
+    context: model.html.videoCanvasCtx,
+    pose: model.pose
   }
 
   args.context.drawImage( args.video, 0, 0, args.canvas.width, args.canvas.height )
 
-  if ( params.input.rotate != 0 ) {
+  if ( model.params.input.rotate != 0 ) {
+
+    // if ( model.params.input.rotate % 180 == 0 ) {
+    //   args.canvas.width = args.video.width;
+    //   args.canvas.height = args.video.height;
+    // } else {
+    //   args.canvas.width = args.video.height;
+    //   args.canvas.height = args.video.width;
+    // }
+
     rotate(
       args.canvas,
       args.canvas,
       args.context,
-      params.input.rotate
+      model.params.input.rotate
     )
   }
 
-  if ( !!params.input.mirror ) {
+  if ( !!model.params.input.mirror ) {
     mirror(
       args.canvas,
       args.canvas,
@@ -188,7 +194,7 @@ async function onFrame() {
     const w = inE.width;
     const h = inE.height;
 
-    const x = outE.width * 0.5;
+    const x = outE.width  * 0.5;
     const y = outE.height * 0.5;
 
     const angleInRadians = angle * ( Math.PI / 180 );
@@ -227,56 +233,57 @@ async function onFrame() {
 
 function onResults( results ) {
 
-  stats.begin();
+  model.stats.begin();
 
   // Clear rect first so the old landmarks are gone if new results are null.
-  canvasCtx.clearRect( 0, 0, canvasElement.width, canvasElement.height );
+  model.html.canvasCtx.clearRect( 0, 0, model.html.canvasElement.width, model.html.canvasElement.height );
 
   if ( results.poseLandmarks == null ) {
-    stats.end();
+    model.stats.end();
     return;
   }
 
-  canvasCtx.save();
+  model.html.canvasCtx.save();
 
-  if ( params.draw.segmentationMask ) {
-    canvasCtx.drawImage( results.segmentationMask, 0, 0,
-      canvasElement.width, canvasElement.height );
+  if ( model.params.draw.segmentationMask ) {
+    model.html.canvasCtx.drawImage( results.segmentationMask, 0, 0,
+      model.html.canvasElement.width, model.html.canvasElement.height );
   }
 
   // Some code from example that draws a mask. Don't really understand what it's doing.
 
   // Only overwrite existing pixels.
-  // canvasCtx.globalCompositeOperation = 'source-in';
-  // canvasCtx.fillStyle = '#0000ff';
-  // canvasCtx.fillRect( 0, 0, canvasElement.width, canvasElement.height );
+  // model.html.canvasCtx.globalCompositeOperation = 'source-in';
+  // model.html.canvasCtx.fillStyle = '#0000ff';
+  // model.html.canvasCtx.fillRect( 0, 0, model.html.canvasElement.width, model.html.canvasElement.height );
 
   // // Only overwrite missing pixels.
-  // canvasCtx.globalCompositeOperation = 'destination-atop';
-  // canvasCtx.drawImage(
-  //   results.image, 0, 0, canvasElement.width, canvasElement.height );
+  // model.html.canvasCtx.globalCompositeOperation = 'destination-atop';
+  // model.html.canvasCtx.drawImage(
+  //   results.image, 0, 0, model.html.canvasElement.width, model.html.canvasElement.height );
 
   // Draw connectors and landmarks
 
-  if ( params.draw.landmarks ) {
-    canvasCtx.globalCompositeOperation = 'source-over';
-    drawUtils.drawConnectors( canvasCtx, results.poseLandmarks, Pose.POSE_CONNECTIONS,
-      { color: '#aaff00', lineWidth: params.draw.connectorSize } );
-    drawUtils.drawLandmarks( canvasCtx, results.poseLandmarks,
-      { color: '#ff0000', lineWidth: params.draw.landmarkSize } );
+  if ( model.params.draw.landmarks ) {
+    model.html.canvasCtx.globalCompositeOperation = 'source-over';
+    drawUtils.drawConnectors( model.html.canvasCtx, results.poseLandmarks, Pose.POSE_CONNECTIONS,
+      { color: '#aaff00', lineWidth: model.params.draw.connectorSize } );
+    drawUtils.drawLandmarks( model.html.canvasCtx, results.poseLandmarks,
+      { color: '#ff0000', lineWidth: model.params.draw.landmarkSize } );
   }
 
-  canvasCtx.restore();
+  model.html.canvasCtx.restore();
 
-  // Send OSC through the function named as a string in params.
-  if ( params.osc.enable && ( ( Date.now() - timer ) > ( 1000 / params.osc.msgsPerSecond ) ) ) {
-
-    window[ params.osc.send_format ]( results );
+  // Send OSC messages in the message rate defined in model.params.
+  if ( model.params.osc.enable && ( ( Date.now() - model.oscTimer ) > ( 1000 / model.params.osc.msgsPerSecond ) ) ) {
+    
+    // Send OSC through the function named as a string in model.params.
+    window[ model.params.osc.send_format ]( results, model.osc );
     timer = Date.now();
 
   }
 
-  stats.end();
+  model.stats.end();
 }
 
 // Input
@@ -327,17 +334,17 @@ function generateGUI( params ) {
   let gui = new dat.GUI();
 
   let folderSrc = gui.addFolder( 'Input' );
-  folderSrc.add( params.input, 'source', params.input.availableSources.video ).name( 'Input Source' ).onChange( ( source ) => getStream( source, videoElement ) );
+  folderSrc.add( params.input, 'source', params.input.availableSources.video ).name( 'Input Source' ).onChange( ( source ) => getStream( source, model.html.videoElement ) );
   folderSrc.add( params.input, 'mirror' );
   folderSrc.add( params.input, 'rotate', 0, 270 ).step( 90 );
   // folderSrc.add( params.input, 'audio' );
 
   let folderPose = gui.addFolder( 'Pose' );
   // { lite: 0, full: 1, heavy: 2 }
-  folderPose.add( params.pose.options, 'modelComplexity', 0, 2 ).step( 1 ).onChange( () => pose.setOptions( params.pose.options ) );
-  folderPose.add( params.pose.options, 'minDetectionConfidence', 0, 1 ).step( 0.01 ).onChange( () => pose.setOptions( params.pose.options ) );
-  folderPose.add( params.pose.options, 'minTrackingConfidence', 0, 1 ).step( 0.01 ).onChange( () => pose.setOptions( params.pose.options ) );
-  // folderPose.add( params.pose.options, 'minDetectionConfidence', 0, 1 ).step( 0.01 ).onChange( () => pose.setOptions( params.pose.options ) );
+  folderPose.add( params.pose.options, 'modelComplexity', 0, 2 ).step( 1 ).onChange( () => model.pose.setOptions( params.pose.options ) );
+  folderPose.add( params.pose.options, 'minDetectionConfidence', 0, 1 ).step( 0.01 ).onChange( () => model.pose.setOptions( params.pose.options ) );
+  folderPose.add( params.pose.options, 'minTrackingConfidence', 0, 1 ).step( 0.01 ).onChange( () => model.pose.setOptions( params.pose.options ) );
+
 
   let folderDraw = gui.addFolder( 'Draw' );
   folderDraw.add( params.draw, 'segmentationMask' )
@@ -387,18 +394,18 @@ function onKeyPress( event ) {
     // C for console.log
     case 'c':
       console.log( {
-        videoElement: videoElement,
+        videoElement: model.html.videoElement,
         drawUtils, drawUtils,
-        camera: camera,
+        camera: model.camera,
         CameraImport: Camera,
-        pose: pose
+        pose: model.pose
       } );
       break;
 
     case 'g':
       dat.GUI.toggleHide();
       params.global.showStats = !params.global.showStats;
-      stats.dom.style.display = params.global.showStats ? "block" : "none";
+      model.stats.dom.style.display = params.global.showStats ? "block" : "none";
       // logDOM.style.display = showStats ? "block" : "none";
       break;
 
@@ -417,7 +424,7 @@ function onKeyPress( event ) {
  * Generates and returns an OSC instance
  * */
 function openOSC( host, port ) {
-  let osc = new OSC( {
+  let oscInstance = new OSC( {
     plugin: new OSC.DatagramPlugin( {
       send: {
         host: host,
@@ -425,8 +432,8 @@ function openOSC( host, port ) {
       }
     } )
   } );
-  osc.open();
-  return osc;
+  oscInstance.open();
+  return oscInstance;
 }
 
 /**
@@ -434,13 +441,14 @@ function openOSC( host, port ) {
  * 
  * @param {Array} poses Array of 33 landmarks organized in objects with x, y, z, and visibility properties - all normalized.
  */
-function sendPosesADDR( poses ) {
+function sendPosesADDR( poses, osc ) {
+
   osc.send( new OSC.Message( '/videoWidth', poses.image.width ) );
   osc.send( new OSC.Message( '/videoHeight', poses.image.height ) );
   //osc.send(new OSC.Message('/nPoses',poses.poseLandmarks.length));
   for ( var i = 0; i < poses.poseLandmarks.length; i++ ) {
     var kpt = poses.poseLandmarks[ i ];
-    var pth = '/pose/keypoints/' + keypointNames[ i ] + "/";
+    var pth = '/pose/keypoints/' + model.keypointNames[ i ] + "/";
 
     osc.send( new OSC.Message( pth + "x", kpt.x ) );
     osc.send( new OSC.Message( pth + "y", kpt.y ) );
@@ -450,11 +458,11 @@ function sendPosesADDR( poses ) {
 
 }
 
-function sendPosesJSON( poses ) {
+function sendPosesJSON( poses, osc ) {
   osc.send( new OSC.Message( "/poses/json", JSON.stringify( poses ) ) );
 }
 
-function sendPosesARR( poses ) {
+function sendPosesARR( poses, osc ) {
   var arr = [ "/poses/arr" ]
   arr.push( camera.videoWidth );
   arr.push( camera.videoHeight );
@@ -471,7 +479,7 @@ function sendPosesARR( poses ) {
   osc.send( new OSC.Message( ...arr ) );
 }
 
-function sendPosesXML( poses ) {
+function sendPosesXML( poses, osc ) {
   function rd( n ) {
     return Math.round( n * 100 ) / 100
   }
